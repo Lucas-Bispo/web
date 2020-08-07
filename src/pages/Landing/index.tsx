af21caeae963180ba19fc6e857f54f472f2bf2c8
Lucas-Bispo <lukaobispo@gmail.com>
2020-08-07 14:04:24 -0300

    salvando dados como rotina, css aplicado

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,9 +7,11 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import './styles.css';
+
 function Landing(){
     return ( 
-    <div id="page-landin">
+    <div id="page-landing">
         <div id="page-landing-content" className="container">
             <div className="logo-container">
                 <img src={logoImg} alt="Proffy"/>
@@ -28,7 +30,7 @@ function Landing(){
 
                 </a>
 
-                <a href="" className="study">
+                <a href="" className="give-classes">
                     <img src={giveClassesIcon} alt="Estudar"/>
                     Estudar
 
