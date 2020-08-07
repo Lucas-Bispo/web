0bf756497e7c7a13620eef8cd13168cb11758d13
Lucas-Bispo <lukaobispo@gmail.com>
2020-08-07 20:09:16 -0300

    pausa

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps{
     title: string;
 }
@@ -22,10 +24,12 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
 
         <div className="header-content">
-        <strong>{props.title}</strong>
+            <strong>{props.title}</strong>
+
+            {props.children}
         </div>
 
-        {props.children}
+        
     </header>
     );
 }
