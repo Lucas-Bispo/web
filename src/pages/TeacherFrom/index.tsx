d4224f208cd141e1b8e59d9e52098ee1eb10a04f
Lucas-Bispo <lukaobispo@gmail.com>
2020-08-07 19:15:51 -0300

    pausa

--- src/pages/TeacherFrom/index.tsx
+++ src/pages/TeacherFrom/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 function TeacherFrom(){
     return(
-        <h1>Teacher From</h1>
+        <div id="page-teacher-form" className="container">
+           <PageHeader title="Que incrivel que você quer dar aulas." />
+        </div>
     )
 }
 
